3550ecf23a483c167ea9faa6770e7ff520a02188
ls9o <thanyakornlesa@gmail.com>
2024-06-17 10:56:48 +0700

    Update project with new code

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <add-info></add-info>
 </template>
+
+<script>
+import AddInfo from "@/components/AddInfo.vue";
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {
+    AddInfo
+  },
+})
+export default class HomeView extends Vue {}
+</script>
