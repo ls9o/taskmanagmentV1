423dde9dbe3c2982064341515a1e2993c5928360
Ls9o <thanyakornlesa@gmail.com>
2024-09-02 13:37:39 +0700

    rollback_1

--- fontend/src/components/ThisNavBar.vue
+++ fontend/src/components/ThisNavBar.vue
@@ -2,13 +2,14 @@
   <!-- lloveanna -->
   <div class="container-fluid">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <div class="col-md-3 mb-2 mb-md-0">
+      <!-- <div class="col-md-3 mb-2 mb-md-0">
         <router-link to="/" custom v-slot="{ href, navigate, isActive }">
+          <i class="fa-solid fa-building-columns"></i>
           <a :href="href" @click="navigate" :class="['d-inline-flex', 'link-body-emphasis', 'text-decoration-none', { 'active': isActive }]">
             <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
           </a>
         </router-link>
-      </div>
+      </div> -->
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li>
